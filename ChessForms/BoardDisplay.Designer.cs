4f3c7d501dd229ac91cfe346d7443907f68d7fbf
Kresten Laust <kressckerl@gmail.com>
2021-01-18 22:02:13 +0100

    forms almost done

--- ChessForms/BoardDisplay.Designer.cs
+++ ChessForms/BoardDisplay.Designer.cs
@@ -35,28 +35,26 @@ namespace ChessForms
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // Form1
+            // BoardDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
+            this.Name = "BoardDisplay";
             this.Text = "Super Skak!";
             this.Load += new System.EventHandler(this.BoardDisplay_Load);
             this.ResumeLayout(false);
--- ChessForms/BoardDisplay.cs
+++ ChessForms/BoardDisplay.cs
@@ -1,11 +1,13 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 using ChessGame;
+using ChessGame.Pieces;
 
 namespace ChessForms
 {
     public partial class BoardDisplay : Form
     {
-        public Chessboard Chessboard;
+        public PictureBox[,] Boardcells;
 
         public BoardDisplay()
         {
@@ -22,28 +24,93 @@ namespace ChessForms
             Player playerWhite = new Player("white");
             Player playerBlack = new Player("black");
 
-            Chessboard = new Chessboard(8, 8, new ChessGame.Gamemodes.ClassicChess(), playerWhite, playerBlack);
-            CreateBoard(8, 8);
+            Chessboard Chessboard = new ChessGame.Gamemodes.ClassicChess().GenerateBoard(playerWhite, playerBlack);
+            CreateBoard(Chessboard.Width, Chessboard.Height);
 
             Chessboard.onGameStateUpdated += Board_onGameStateUpdated;
 
             Chessboard.StartGame();
         }
 
+        public void UpdateBoard(Chessboard board)
+        {
+            for (int y = 0; y < board.Height; y++)
+            {
+                for (int x = 0; x < board.Width; x++)
+                {
+
+                }
+            }
+        }
+
         public void CreateBoard(int width, int height)
         {
             tableLayoutPanel1.ColumnCount = width;
+            tableLayoutPanel1.ColumnStyles.Clear();
+            for (int i = 0; i < width; i++)
+            {
+                // set size to any percent, doesnt matter
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 1));
+            }
+
             tableLayoutPanel1.RowCount = height;
+            tableLayoutPanel1.RowStyles.Clear();
+            for (int i = 0; i < height; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new ColumnStyle(SizeType.Percent, 1));
+            }
+
+            Boardcells = new PictureBox[width, height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    PictureBox box = new PictureBox
+                    {
+                        Dock = DockStyle.Fill,
+                        Image = Image.FromFile(@"C:\Users\kress\Documents\geotermi.jpg")
+                    };
+
+                    box.Click += CellClicked;
+                    Boardcells[x, y] = box;
+
+                    tableLayoutPanel1.Controls.Add(box);
+                }
+            }
         }
 
-        public void ClearPiece()
+        private void CellClicked(object sender, System.EventArgs e)
         {
-
+            
         }
 
-        public void PlacePiece()
+        public void ClearPiece(int x, int y)
         {
+            Boardcells[x, y].Image = null;
+        }
 
+        public void PlacePiece(int x, int y, Piece piece)
+        {
+            Boardcells[x, y].Image = GetPieceImage(piece);
+        }
+
+        private Image GetPieceImage(Piece piece)
+        {
+            switch (piece)
+            {
+                case Bishop _:
+                    break;
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+        private void pictureBox1_Click(object sender, System.EventArgs e)
+        {
+            
         }
     }
 }
