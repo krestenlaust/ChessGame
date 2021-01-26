cc94f35b583cdcf42ef9a5542a46c544eba7ba1d
Kresten Laust <kressckerl@gmail.com>
2021-01-26 09:32:02 +0100

    Removed new TreeNode system because it had issues with enourmous overhead (up to 2-3 gb memory usage)

--- ChessGame/Players/Skakinator.cs
+++ ChessGame/Players/Skakinator.cs
@@ -1,242 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChessGame.Players
 {
     public class Skakinator : Player
     {
-        private TreeNode rootNode;
-
         public Skakinator(string name) : base(name)
         {
         }
 
-        private class TreeNode
+        private int MinimaxSearch(Chessboard board, int depth, int alpha, int beta)
         {
-            public static Dictionary<int, TreeNode> TranspositionTable = new Dictionary<int, TreeNode>();
-            public readonly Chessboard Chessboard;
-            public int? Value = null;
-            private readonly Dictionary<Move, TreeNode> children;
-
-            public bool Maximize
+            if (board.CurrentState == GameState.Checkmate)
             {
-                get
-                {
-                    return Chessboard.CurrentTeamTurn == TeamColor.White;
-                }
-            }
-            public bool LeafNode
-            {
-                get
-                {
-                    switch (Chessboard.CurrentState)
-                    {
-                        case GameState.Stalemate:
-                        case GameState.Checkmate:
-                            return true;
-                    }
-
-                    return false;
-                }
+                // note: the turn is updated even after checkmate
+                return (depth + 1) * (board.CurrentTeamTurn == TeamColor.Black ? short.MaxValue : short.MinValue);
             }
 
-            public TreeNode(Chessboard board)
+            if (board.CurrentState == GameState.Stalemate)
             {
-                children = new Dictionary<Move, TreeNode>();
-                Chessboard = board;
-                Value = null;
+                return 0;
             }
 
-            public TreeNode this[Move move]
+            if (depth == 0)
             {
-                get { return children[move]; }
-                set { children[move] = value; }
+                return board.MaterialSum;
             }
 
-            public IEnumerable<TreeNode> GetChildren()
-            {
-                if (children.Count == 0)
-                {
-                    foreach (var move in Chessboard.GetMoves())
-                    {
-                        Chessboard newBoard = new Chessboard(Chessboard, move);
-                        
-                        TreeNode node = new TreeNode(newBoard);
-
-                        children[move] = node;
-                        yield return node;
-                    }
-                }
-                else
-                {
-                    foreach (var node in children.Values)
-                    {
-                        yield return node;
-                    }
-                }
-            }
-
-            public int Evaluation(int depth)
-            {
-                if (Chessboard.CurrentState == GameState.Checkmate)
-                {
-                    // note: the turn is updated even after checkmate
-                    return (depth + 1) * (Chessboard.CurrentTeamTurn == TeamColor.Black ? short.MaxValue : short.MinValue);
-                }
-
-                if (Chessboard.CurrentState == GameState.Stalemate)
-                {
-                    return 0;
-                }
-
-                return Chessboard.MaterialSum;
-            }
-        }
-
-        private int MinimaxSearch(TreeNode node, int depth, int alpha, int beta)
-        {
-            // Return node value if it's a leaf node.
-            if (depth == 0 || node.LeafNode)
-            {
-                return node.Evaluation(depth);
-            }
-
-            int bestEvaluation = node.Maximize ? int.MinValue : int.MaxValue;
-
-            foreach (var childNode in node.GetChildren())
-            {
-                int hashCode = childNode.Chessboard.GetHashCode();
-                int evaluation;
-                if (TreeNode.TranspositionTable.TryGetValue(hashCode, out TreeNode precalculatedChildNode))
-                {
-                    if (precalculatedChildNode.Value is null)
-                    {
-                        evaluation = MinimaxSearch(childNode, depth - 1, alpha, beta);
-                    }
-                    else
-                    {
-                        evaluation = precalculatedChildNode.Value.Value;
-                    }
-                }
-                else
-                {
-                    evaluation = MinimaxSearch(childNode, depth - 1, alpha, beta);
-                    TreeNode.TranspositionTable[hashCode] = childNode;
-                }
-
-                if (node.Maximize)
-                {
-                    bestEvaluation = Math.Max(bestEvaluation, evaluation);
-                    alpha = Math.Max(alpha, bestEvaluation);
-                }
-                else
-                {
-                    bestEvaluation = Math.Min(bestEvaluation, evaluation);
-                    alpha = Math.Min(alpha, bestEvaluation);
-                }
-
-                if (beta <= alpha)
-                {
-                    break;
-                }
-            }
-
-            node.Value = bestEvaluation;
-            return bestEvaluation;
-        }
-
-        public override void TurnStarted(Chessboard board)
-        {
-            // first move
-            if (rootNode is null)
-            {
-                rootNode = new TreeNode(board);
-            }
-            else
-            {
-                // move child node up tree and discard siblings.
-                rootNode = rootNode[board.Moves.Peek()];
-                GC.Collect();
-            }
-
-            TreeNode.TranspositionTable.Clear();
-
-            int bestEvaluation = rootNode.Maximize ? int.MinValue : int.MaxValue;
-
-            List<(Move, int)> evaluatedMoves = new List<(Move, int)>();
-            foreach (var move in board.GetMoves())
-            {
-                Chessboard newBoard = new Chessboard(board, move);
-                TreeNode node = new TreeNode(newBoard);
-
-                int evaluation = MinimaxSearch(node, 3, int.MinValue, int.MaxValue);
-
-                if (rootNode.Maximize && evaluation < bestEvaluation)
-                {
-                    node = null;
-                    continue;
-                }
-
-                if (!rootNode.Maximize && evaluation >= bestEvaluation)
-                {
-                    node = null;
-                    continue;
-                }
-
-                rootNode[move] = node;
-
-                bestEvaluation = evaluation;
-
-                evaluatedMoves.Add(
-                    (move, evaluation)
-                );
-            }
-
-            List<Move> sortedMoves;
             if (board.CurrentTeamTurn == TeamColor.White)
-            {
-                sortedMoves = (from moveEvaluation in evaluatedMoves
-                               orderby moveEvaluation.Item2 descending
-                               select moveEvaluation.Item1).ToList();
-            }
-            else
-            {
-                sortedMoves = (from moveEvaluation in evaluatedMoves
-                               orderby moveEvaluation.Item2 ascending
-                               select moveEvaluation.Item1).ToList();
-            }
-
-            // select move
-            Move chosenMove = sortedMoves[1];
-
-            // discard sibling moves now that one has been chosen.
-            rootNode = rootNode[chosenMove];
-
-            board.PerformMove(chosenMove);
-            GC.Collect();
-        }
-    }
-}
-
-/*
-            List<(int, Move)> moves = new List<(int, Move)>();
-            foreach (var move in board.GetMoves())
-            {
-                Chessboard rootNode = new Chessboard(board, move);
-
-                moves.Add((MinimaxSearch(rootNode, 2, int.MinValue, int.MaxValue), move));
-            }*/
-
-/*if (node.Maximize)
             {
                 int maxEvaluation = int.MinValue;
 
-                foreach (var move in node.Chessboard.GetMoves())
+                foreach (var move in board.GetMoves())
                 {
-                    node[move] = new TreeNode(new Chessboard(node.Chessboard, move));
+                    Chessboard childNode = new Chessboard(board, move);
 
-                    maxEvaluation = Math.Max(maxEvaluation, MinimaxSearch(node[move], depth - 1, alpha, beta));
+                    maxEvaluation = Math.Max(maxEvaluation, MinimaxSearch(childNode, depth - 1, alpha, beta));
                     alpha = Math.Max(alpha, maxEvaluation);
 
                     // a better move was found earlier.
@@ -246,18 +48,17 @@ namespace ChessGame.Players
                     }
                 }
 
-                node.Value = maxEvaluation;
                 return maxEvaluation;
             }
             else
             {
                 int minEvaluation = int.MaxValue;
 
-                foreach (var move in node.Chessboard.GetMoves())
+                foreach (var move in board.GetMoves())
                 {
-                    Chessboard childNode = new Chessboard(node.Chessboard, move);
+                    Chessboard childNode = new Chessboard(board, move);
 
-                    minEvaluation = Math.Min(minEvaluation, MinimaxSearch(new TreeNode(childNode), depth - 1, alpha, beta));
+                    minEvaluation = Math.Min(minEvaluation, MinimaxSearch(childNode, depth - 1, alpha, beta));
                     beta = Math.Min(beta, minEvaluation);
 
                     if (beta <= alpha)
@@ -266,6 +67,36 @@ namespace ChessGame.Players
                     }
                 }
 
-                node.Value = minEvaluation;
                 return minEvaluation;
-            }*/
+            }
+        }
+
+        public override void TurnStarted(Chessboard board)
+        {
+            List<(int, Move)> moves = new List<(int, Move)>();
+            foreach (var move in board.GetMoves())
+            {
+                Chessboard rootNode = new Chessboard(board, move);
+
+                moves.Add((MinimaxSearch(rootNode, 3, int.MinValue, int.MaxValue), move));
+            }
+
+            List<Move> sortedMoves;
+            if (board.CurrentTeamTurn == TeamColor.White)
+            {
+                sortedMoves = (from moveEvaluation in moves
+                               orderby moveEvaluation.Item1 descending
+                               select moveEvaluation.Item2).ToList();
+            }
+            else
+            {
+                sortedMoves = (from moveEvaluation in moves
+                               orderby moveEvaluation.Item1 ascending
+                               select moveEvaluation.Item2).ToList();
+            }
+
+
+            board.PerformMove(sortedMoves[0]);
+        }
+    }
+}
