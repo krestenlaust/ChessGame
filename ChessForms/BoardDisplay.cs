5442dc5fdc90a3c8e5fa74db6c6756ee2c1ca717
Kresten Laust <kressckerl@gmail.com>
2021-02-11 16:58:43 +0100

    Random things, changed language version to preview

--- ChessForms/BoardDisplay.cs
+++ ChessForms/BoardDisplay.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ChessForms
